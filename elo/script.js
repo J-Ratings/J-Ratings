57d73e4cf6f5fe73d78f729f0956f18c0238f438
WeniMakOfWay <107587470+WeniMakOfWay@users.noreply.github.com>
2025-05-07 15:42:48 +0100

    Update script.js

--- elo/script.js
+++ elo/script.js
@@ -38,12 +38,12 @@ Plotly.d3.csv("plot_data.csv", function(err, rows) {
 
   // 3. Define an extended colour palette
   const customColors = [
-    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+    "#1f77b4", "#de9ed6", "#ff7f0e", "#2ca02c", "#d62728", 
+    "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
     "#393b79", "#637939", "#8c6d31", "#843c39", "#7b4173",
     "#5254a3", "#6b6ecf", "#9c9ede", "#8ca252", "#bd9e39",
     "#e7ba52", "#e7969c", "#d6616b", "#ad494a", "#a55194",
-    "#ce6dbd", "#de9ed6", "#6b6ecf", "#9c9ede", "#ff9896",
+    "#ce6dbd", "#6b6ecf", "#9c9ede", "#ff9896",
     "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5",
     "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff7f0e",
     "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94"
