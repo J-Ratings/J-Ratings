b93d5e84f1780c4065927bcff3dcafa5b67b47b7
WeniMakOfWay <107587470+WeniMakOfWay@users.noreply.github.com>
2025-06-05 18:12:12 +0100

    Update script.js

--- elo/script.js
+++ elo/script.js
@@ -58,7 +58,7 @@ Plotly.d3.csv("plot_data.csv", function(err, rows) {
       mode: 'lines',
       name: name,
       line: { color: customColors[index % customColors.length] },
-      hovertemplate: '%{y:.0f}<extra>%{fullData.name}</extra>'
+      hovertemplate: 'Date: %{x|%Y-%m-%d}<br>ELO: %{y:.0f}<extra>%{fullData.name}</extra>'
     };
   });
 
